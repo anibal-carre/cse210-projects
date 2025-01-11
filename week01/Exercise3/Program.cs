1b51e9436ed91275d77fed9fa4d53e7ed6bca9e7
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-01-11 10:11:54 -0300

    loops exercise

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGen = new();
+        // Studying and with the warnings of visual studio code, I discovered that you can use this abbreviation, instead of using new Random()
+
+
+        int magicNumber = randomGen.Next(1, 101);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
