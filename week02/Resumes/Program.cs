c7d52107e73b791dec04958565e8f793c2a47a08
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-01-14 09:53:07 -0300

    abstraction exercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,33 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job
+        {
+            _jobTitle = "Telemarketing Agent",
+            _company = "Atento Brazil",
+            _startYear = 2023,
+            _endYear = 2024
+        };
+
+        Job job2 = new Job
+        {
+            _jobTitle = "Content Moderator",
+            _company = "Facebook",
+            _startYear = 2024,
+            _endYear = 2025
+        };
+
+        Resume myResume = new Resume
+        {
+            _name = "Anibal David",
+            _jobs = []
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
