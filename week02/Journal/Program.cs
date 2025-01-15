8148be9e5bbac70dcd5b0ccf7ed6d636d3f588c1
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-01-15 09:51:55 -0300

    working on the Journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        List<string> menuOptions = ["1. Write", "2. Display", "3. Load", "4. Save", "5. Quit"];
+        List<string> questions = ["Who surprised me the most with their perspective or actions today?", "What moment today filled me with the most joy or pride?", "How did I notice God’s guidance or blessings in unexpected ways today?", "What emotion stood out as the most powerful or meaningful for me today, and why?", "If I could relive one part of today to do it differently or savor it more, what would it be?"];
+
+        Console.WriteLine("Please select one of the following choices: ");
+        foreach (string option in menuOptions)
+        {
+            Console.WriteLine($"{option}");
+        }
+        Console.Write("What would you like to do? ");
+
+        string userResponse = Console.ReadLine();
+        int selectedOption = int.Parse(userResponse);
+
+        Console.WriteLine(selectedOption);
     }
 }
