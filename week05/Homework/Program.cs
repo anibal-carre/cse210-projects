693070894ee3499ba6e7efbf6c7e5581ac73e634
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-02-04 22:35:11 -0300

    Program Class done

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Joh Carry", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Carlos", "Fractions", "8.4", "5-15");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+
+        WritingAssigment writingAssigment = new WritingAssigment("Carlos Alberto", "Brazilian History", "The 80's in Brazil");
+        Console.WriteLine(writingAssigment.GetSummary());
+        Console.WriteLine(writingAssigment.GetWritingInformation());
     }
 }
