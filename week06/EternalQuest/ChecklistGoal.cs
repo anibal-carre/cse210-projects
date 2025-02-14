e6fb82ecf8a1f61c0fff584570e4e56a3127e773
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-02-13 22:33:41 -0300

    Goal Manager

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -1,4 +1,31 @@
-public class ChecklistGoal
+public class ChecklistGoal : Goal
 {
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, string points, int target, int bonus) : base(name, description, points)
+    {
+        _target = target;
+        _bonus = bonus;
+        _amountCompleted = 0;
+    }
+
+    public override void RecordEvent()
+    {
+
+    }
+
+    public override bool IsComplete()
+    {
+        return true;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+
+
 
 }
