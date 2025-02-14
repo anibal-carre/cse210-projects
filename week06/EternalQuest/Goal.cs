545d721038adaf567f2ea95305b7a9f5659e6742
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-02-13 21:11:55 -0300

    Goal Class variables and methods

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,4 +1,33 @@
 public class Goal
 {
+    private string _shortName;
+    private string _description;
+    private string _points;
 
+    public Goal(string shortName, string description, string points)
+    {
+        _shortName = shortName;
+        _description = description;
+        _points = points;
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public bool isComplete()
+    {
+        return true;
+    }
+
+    public string GetDetailsString()
+    {
+        return "";
+    }
+
+    public string GetStringRepresentation()
+    {
+        return "";
+    }
 }
