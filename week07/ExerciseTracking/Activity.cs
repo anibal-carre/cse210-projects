d20aa1d790d4989045b24f3b48d32cb43633fa33
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-02-18 09:23:03 -0300

    Work in Activity Class GetSummary method

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,4 +1,27 @@
-public class Activity
+public abstract class Activity(int minutes, DateTime date)
 {
+    private int _minutes = minutes;
+    private DateTime _date = date;
 
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public abstract double GetDistance();
+
+    public abstract double GetSpeed();
+
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+
+        return $"{_date.ToString("dd MM yyyy")}";
+    }
 }
